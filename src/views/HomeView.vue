7144a5159b62f9b6ffd43ccabb5e36061470e864
jaspercloud <qq276726581@gmail.com>
2023-10-17 16:34:59 +0800

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,14 +9,18 @@
 </style>
 
 <template>
-  <el-container class="page">
-    <el-header style="background-color: #545c64;">
-      <div class="flex justify-center">
-        <el-menu :default-active="menuCode" @select="handleSelect" mode="horizontal" background-color="#545c64"
-          text-color="#fff" active-text-color="#409EFF" :ellipsis="false">
-          <el-menu-item index="node" style="width: 120px;">节点</el-menu-item>
-          <el-menu-item index="route" style="width: 120px;">路由</el-menu-item>
-        </el-menu>
+  <el-container class="page" style="background-color: #f4f4f5;">
+    <el-header style="background-color: #FFFFFF;" class="flex">
+      <div style="position: relative;" class="flex flex-item">
+        <div style="position: absolute;width: 100%;height: 100%;" class="flex align-center">
+          <img src="/banner.png" alt="" style="height: 60px;">
+        </div>
+        <div style="position: absolute;width: 100%;" class="flex justify-center">
+          <el-menu :default-active="menuCode" @select="handleSelect" mode="horizontal" :ellipsis="false">
+            <el-menu-item index="node" style="width: 120px;">节点</el-menu-item>
+            <el-menu-item index="route" style="width: 120px;">路由</el-menu-item>
+          </el-menu>
+        </div>
       </div>
     </el-header>
     <el-main>
