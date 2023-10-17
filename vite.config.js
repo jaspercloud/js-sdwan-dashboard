6e2a2e88d82435f16aa62f76103573b211ec8e26
jaspercloud <qq276726581@gmail.com>
2023-10-17 14:08:05 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+        changeOrigin: true
       }
     }
   }
