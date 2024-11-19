4427e95f2fbfdac24ed7e4c82e2d918952e0343c
jaspercloud <qq276726581@gmail.com>
2024-11-19 16:33:58 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/normalize.css'
 import './assets/flex.css'
 import './assets/base.css'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -12,3 +13,8 @@ const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+
