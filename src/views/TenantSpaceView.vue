0d109ab361bf4ce1827a3cc78b1928c4262d4018
jaspercloud <qq276726581@gmail.com>
2024-11-20 13:57:56 +0800

    update

--- src/views/TenantSpaceView.vue
+++ src/views/TenantSpaceView.vue
@@ -1,28 +1,33 @@
 <template>
     <el-container style="height: 100%;">
-        <el-aside style="height: 100%;" class="flex direction-column">
-            <h3 style="margin-left: 20px">{{ tenant.name }}</h3>
-            <el-menu :default-active="onRoutes" router="true" class="flex-item">
-                <el-menu-item index="/tenantSpace/node">
-                    <span>节点管理</span>
-                </el-menu-item>
-                <el-menu-item index="/tenantSpace/route">
-                    <span>路由管理</span>
-                </el-menu-item>
-                <el-menu-item index="/tenantSpace/rule">
-                    <span>规则管理</span>
-                </el-menu-item>
-                <el-menu-item index="/tenantSpace/vnat">
-                    <span>地址转换</span>
-                </el-menu-item>
-                <el-menu-item index="/tenantSpace/group">
-                    <span>分组管理</span>
-                </el-menu-item>
-            </el-menu>
-        </el-aside>
-        <el-main>
-            <RouterView class="flex-item" style="height: 100%;" />
-        </el-main>
+        <el-header style="background-color: #409EFF;">
+            <h3 style="margin-left: 20px;color: #ffffff;">{{ tenant.name }}</h3>
+        </el-header>
+        <el-container>
+            <el-aside style="height: 100%;" class="flex direction-column">
+                <el-menu :default-active="onRoutes" router="true" class="flex-item">
+                    <el-menu-item index="/tenantSpace/node">
+                        <span>节点管理</span>
+                    </el-menu-item>
+                    <el-menu-item index="/tenantSpace/route">
+                        <span>路由管理</span>
+                    </el-menu-item>
+                    <el-menu-item index="/tenantSpace/rule">
+                        <span>规则管理</span>
+                    </el-menu-item>
+                    <el-menu-item index="/tenantSpace/vnat">
+                        <span>地址转换</span>
+                    </el-menu-item>
+                    <el-menu-item index="/tenantSpace/group">
+                        <span>分组管理</span>
+                    </el-menu-item>
+                </el-menu>
+                <el-button size="large">退出</el-button>
+            </el-aside>
+            <el-main>
+                <RouterView class="flex-item" style="height: 100%;" />
+            </el-main>
+        </el-container>
     </el-container>
 </template>
 <script>
