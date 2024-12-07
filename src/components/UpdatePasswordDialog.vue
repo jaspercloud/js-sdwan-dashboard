87403ad467f98b9629317932f41f31f99066e9be
jaspercloud <qq276726581@gmail.com>
2024-12-07 16:19:58 +0800

    add updatepwd

---
+++ src/components/UpdatePasswordDialog.vue
@@ -0,0 +1,82 @@
+<template>
+    <el-dialog v-model="updatePwdDialog.visible" title="修改密码" width="500" :close-on-click-modal="false"
+        :before-close="onClosed">
+        <el-form :model="updatePwdDialog.form" label-position="right" label-width="auto">
+            <el-form-item label="当前密码">
+                <el-input v-model="updatePwdDialog.form.password" type="password" show-password="true"
+                    autocomplete="new-password" />
+            </el-form-item>
+            <el-form-item label="新密码">
+                <el-input v-model="updatePwdDialog.form.newPassword" type="password" show-password="true"
+                    autocomplete="new-password" />
+            </el-form-item>
+            <el-form-item label="确认密码">
+                <el-input v-model="updatePwdDialog.form.conformPassword" type="password" show-password="true"
+                    autocomplete="new-password" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="updatePassword">
+                    确定
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
+</template>
+<style></style>
+<script>
+import { ElMessage } from 'element-plus';
+import http from '../api';
+export default {
+    props: ["modelValue"],
+    data() {
+        return {
+            updatePwdDialog: {
+                visible: false,
+                form: {
+                    password: "",
+                    conformPassword: "",
+                    newPassword: ""
+                }
+            }
+        };
+    },
+    mounted() {
+        if (null != this.modelValue) {
+            this.updatePwdDialog.visible = this.modelValue
+        }
+    },
+    updated() {
+        if (null != this.modelValue) {
+            this.updatePwdDialog.visible = this.modelValue
+        }
+    },
+    methods: {
+        onClosed(done) {
+            this.updatePwdDialog.form = {
+                password: "",
+                conformPassword: "",
+                newPassword: ""
+            }
+            this.$emit("update:modelValue", false)
+            done()
+        },
+        async updatePassword() {
+            let v1 = this.updatePwdDialog.form.newPassword
+            let v2 = this.updatePwdDialog.form.conformPassword
+            if (v1 != v2) {
+                ElMessage.error("两次密码不一致")
+                return
+            }
+            let { status, data } = await http.post(`/api/account/updatePassword`, this.updatePwdDialog.form)
+            if (200 != status) {
+                ElMessage.error(data)
+                return
+            }
+            ElMessage.success("修改密码成功")
+            this.$emit("update:modelValue", false)
+        }
+    }
+};
+</script>
--- src/views/TenantSpaceView.vue
+++ src/views/TenantSpaceView.vue
@@ -1,7 +1,26 @@
 <template>
     <el-container style="height: 100%;">
         <el-header style="background-color: #409EFF;">
-            <h3 style="margin-left: 20px;color: #ffffff;">{{ tenant.name }}</h3>
+            <el-row>
+                <el-col :span="12">
+                    <h3 style="margin-left: 20px;color: #ffffff;">{{ tenant.name }}</h3>
+                </el-col>
+                <el-col :span="12" class="flex-item direction-row-reverse align-center">
+                    <el-dropdown @command="clickMenu">
+                        <el-button circle size="large">
+                            <el-icon>
+                                <User />
+                            </el-icon>
+                        </el-button>
+                        <template #dropdown>
+                            <el-dropdown-menu>
+                                <el-dropdown-item command="openUpdatePwdDialog">修改密码</el-dropdown-item>
+                                <el-dropdown-item command="exit">退出</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </template>
+                    </el-dropdown>
+                </el-col>
+            </el-row>
         </el-header>
         <el-container>
             <el-aside style="height: 100%;" class="flex direction-column">
@@ -22,21 +41,33 @@
                         <span>分组管理</span>
                     </el-menu-item>
                 </el-menu>
-                <el-button size="large" @click="exit">退出</el-button>
             </el-aside>
             <el-main>
                 <RouterView class="flex-item" style="height: 100%;" />
             </el-main>
         </el-container>
     </el-container>
+    <update-password-dialog v-model="updatePasswordDialogVisible" />
 </template>
+<style scoped>
+.el-col-12 {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: flex;
+}
+</style>
 <script>
 import http from '../api';
+import UpdatePasswordDialog from '../components/UpdatePasswordDialog.vue';
 export default {
+    components: {
+        "update-password-dialog": UpdatePasswordDialog
+    },
     data() {
         return {
             onRoutes: this.$route.path,
-            tenant: {}
+            tenant: {},
+            updatePasswordDialogVisible: false
         };
     },
     watch: {
@@ -54,8 +85,14 @@ export default {
         this.tenant = data
     },
     methods: {
+        openUpdatePwdDialog() {
+            this.updatePasswordDialogVisible = true
+        },
         exit() {
             this.$router.replace("/")
+        },
+        clickMenu(command) {
+            console.log(this[command]())
         }
     }
 };
